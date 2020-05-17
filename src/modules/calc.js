4b403f3b95158a955ae6d14dc246dded68b52636
Ilya <chernyi.ilya.00@gmail.com>
2020-05-17 23:02:00 +0300

    fix: fixed calculator's work

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,23 +1,85 @@
 const calc = () => {
-    const switchCam = document.querySelector('.onoffswitch-label'),
-        switchInput = document.querySelector('.onoffswitch-checkbox'),
-        collapseTwo = document.querySelector('#collapseTwo'),
-        collapseThree = document.querySelector('#collapseThree'),
-        collapseFour = document.querySelector('#collapseFour'),
-        second = document.querySelector('.second');
+    const switchCam = document.querySelector('.onoffswitch-label.label-type'),
+        switchBottom = document.querySelector('.onoffswitch-label.label-bottom'),
+        switchCamInput = document.querySelector('.onoffswitch-checkbox.input-type'),
+        switchBottomInput = document.querySelector('.onoffswitch-checkbox.input-bottom'),
+        collapseTwo = document.getElementById('collapseTwo'),
+        collapseThree = document.getElementById('collapseThree'),
+        collapseFour = document.getElementById('collapseFour'),
+        calcResult = document.getElementById('calc-result'),
+        second = document.querySelector('.second'),
+        first = document.querySelector('.first'),
+        selects = document.querySelector('.constructor').querySelectorAll('select'),
+        inputs = document.querySelector('.constructor').querySelectorAll('input');
     second.style.display = 'none'
     collapseTwo.classList.toggle('in');
     collapseThree.classList.toggle('in');
     collapseFour.classList.toggle('in');
-    switchCam.addEventListener('click', () => { // если true, то двухкамерный
-        if (switchInput.hasAttribute('checked')) {
-            switchInput.removeAttribute('checked');
-            second.style.display = 'none';
-        } else {
-            switchInput.setAttribute('checked', true);
+    let flag = true;
+    let flag2 = true;
+    const recount = () => {
+        let basicPrice = 10000;
+        if (!flag) {
+            basicPrice = 15000;
+        }
+        let counter = basicPrice;
+        counter += basicPrice * (first.querySelector('.diam').value == '2' ? 0.2 : 0);
+        if (first.querySelector('.count').value == '2') {
+            counter += basicPrice * 0.3;
+        } else{
+            if (first.querySelector('.count').value == '3') {
+                counter += basicPrice * 0.5;
+            }
+        }
+        if (!flag) {
+            counter += basicPrice * (second.querySelector('.diam').value == '2' ? 0.2 : 0);
+            if (second.querySelector('.count').value == '2') {
+                counter +=basicPrice * 0.3;
+            } else{
+                if (second.querySelector('.count').value == '3') {
+                    counter +=basicPrice * 0.5;
+                }
+            }
+        }
+        if (flag2) {
+            if (flag2 && !flag) {
+                counter += 1000;
+            }
+            counter += 1000;
+        }
+        calcResult.value = counter;
+    };
+    switchCam.addEventListener('click', () => {
+        if (flag) {
+            flag = false;
             second.style.display = 'block';
+            recount();
+        } else {
+            flag = true;
+            second.style.display = 'none';
+            recount();
         }
     })
+    switchBottom.addEventListener('click', () => {
+        if (flag2) {
+            flag2 = false;
+            recount();
+        } else {
+            flag2 = true;
+            recount()
+        }
+    })
+    recount();
+    selects.forEach((elem) => {
+        elem.addEventListener('change', recount)
+    });
 }
-
 calc();
+
+// const accordion = document.querySelector('#accordion');
+// accordion.addEventListener('click', e => {
+//   const target = e.target;
+//   if(target.closest('#myonoffswitch')) {
+//     (target.checked) ? console.log(1) : console.log(0); 
+//   }
+// });
