3aebc2871021f84246af2781dae595acc4523996
Ilya <chernyi.ilya.00@gmail.com>
2020-05-17 16:05:59 +0300

    feat: made the second block in the second section of calculator appear and disappear

---
+++ src/modules/calc.js
@@ -0,0 +1,23 @@
+const calc = () => {
+    const switchCam = document.querySelector('.onoffswitch-label'),
+        switchInput = document.querySelector('.onoffswitch-checkbox'),
+        collapseTwo = document.querySelector('#collapseTwo'),
+        collapseThree = document.querySelector('#collapseThree'),
+        collapseFour = document.querySelector('#collapseFour'),
+        second = document.querySelector('.second');
+    second.style.display = 'none'
+    // collapseTwo.classList.toggle('in');
+    // collapseThree.classList.toggle('in');
+    // collapseFour.classList.toggle('in');
+    switchCam.addEventListener('click', () => { // если true, то двухкамерный
+        if (switchInput.hasAttribute('checked')) {
+            switchInput.removeAttribute('checked');
+            second.style.display = 'none';
+        } else {
+            switchInput.setAttribute('checked', true);
+            second.style.display = 'block';
+        }
+    })
+}
+
+calc();
